101cbd84d3a4f90a41e4d045c695f68f590c2726
John Fred Fadrigalan <johnfred.fadrigalan@hotmail.com>
2024-11-11 07:38:50 +0800

    chore(dependencies) add angular/material theme

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { MatTableModule } from '@angular/material/table';
+
+
 @Component({
   selector: 'app-root',
   standalone: true,
